fc1f6a922f245e3ffb509ae94d2c43d7e4ed8992
Eun Jeong Oh <136856816+oheunj@users.noreply.github.com>
2025-03-08 19:23:21 -0500

    Update tpsurv_main_code.R

--- tpsurv_main_code.R
+++ tpsurv_main_code.R
@@ -18,7 +18,7 @@ library(knitr)
 # - nIter: # of Monte Carlo simulations
 # - case: 1 (a binary missing covariate); 2 (a continuous missing covariate); 3 (two missing covariates)
 # - setting: 1 (MCAR); 2 (MAR); 3 (MARviol)
-# - n: # of observations in two-phase data (n >> n')
+# - n: # of total observations in two-phase data
 # - p: dimension of U
 # - alpha: coefficient(s) of V
 # - beta: coefficients of U
